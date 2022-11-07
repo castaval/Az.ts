44fdd7957d4f29f3ba3373cfb110e0c3a8f6f384
castaval <alexander.butusov.03@mail.ru>
2022-11-07 11:31:02 +0300

    wtf

---
+++ types/az.dawg.d.ts
@@ -0,0 +1,7 @@
+declare module "az.dawg" {
+    export function offset(base: number): number;
+    export function label(base: number): number;
+    export function hasLeaf(base: number): number;
+    export function value(base: number): number;
+}
+
